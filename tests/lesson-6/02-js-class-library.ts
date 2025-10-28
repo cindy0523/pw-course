5a50167bde4b00faecd7baffdbf8c4e01536d42d
Thu <minhthuphan2002@gmail.com>
2025-10-28 23:40:53 +0700

    feat: bai tap lesson 6 part 2

--- tests/lesson-6/02-js-class-library.ts
+++ tests/lesson-6/02-js-class-library.ts
@@ -7,3 +7,42 @@ Yêu cầu:
 - Tạo một phương thức addBook để thêm sách vào thư viện.
 - Tạo một phương thức findBook để tìm sách theo tiêu đề.
 */
+
+class Book {
+    bookId: number;
+    bookName: string;
+
+    constructor(bookId: number, bookName: string) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+    };
+};
+
+class Library {
+    name: string;
+    location: string;
+    books: Book[];
+
+    constructor(name: string, location: string) {
+        this.name = name;
+        this.location = location;
+        this.books = [];
+    };
+
+    //thêm sách
+    addBook(newBook: Book) {
+        this.books.push(newBook);
+    };
+
+    //tìm sách thep title
+    findBook(bookName: string): Book | undefined {
+        return this.books.find(book => book.bookName.toLowerCase() === bookName.toLowerCase());
+    };
+};
+
+let myLibrary = new Library ("Thư viện Cute", "HCM");
+
+myLibrary.addBook(new Book(2, "Sách toán"));
+myLibrary.addBook(new Book(3, "Sách văn"));
+
+console.log(myLibrary.findBook("Sách toán"));
--- tests/lesson-6/02-js-class-order.ts
+++ tests/lesson-6/02-js-class-order.ts
@@ -10,3 +10,45 @@ Yêu cầu:
 - Tạo một phương thức addItem để thêm sản phẩm vào đơn hàng.
 - Tạo một phương thức calculateTotal để tính tổng số tiền của đơn hàng.
 */
+
+class Product {
+    name: string;
+    price: number;
+    amount: number;
+    discount: number;
+
+    constructor(name: string, price: number, amount: number, discount: number= 0) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.discount = discount; //phần trăm giảm giá
+    };
+};
+
+class Order {
+    orderId: number;
+    customerName: string;
+    items: Product[];
+    totalAmount: number;
+
+    constructor(orderId: number, customerName: string) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.items = [];
+        this.totalAmount = 0;
+    };
+
+    // phương thức addItem để thêm sản phẩm vào đơn hàng.
+    addItem(newItem: Product) {
+        this.items.push(newItem);
+    };
+    // phương thức calculateTotal để tính tổng số tiền của đơn hàng.
+    calculateTotal() {
+        return 
+    };
+};
+
+let order1 = new Order(1, "Andy");
+
+order1.addItem(new Product("notebook", 100, 2, 0));
+order1.addItem(new Product("dress", 200, 1, 0));
