6fe2255af2382ca69ad1c312d13fda41ec9fcc2f
Thu <minhthuphan2002@gmail.com>
2025-12-24 13:33:55 +0700

    fix: refactor code bài tập lesson 11

--- tests/lesson-11-api/api-test1.spec.ts
+++ tests/lesson-11-api/api-test1.spec.ts
@@ -1,23 +1,126 @@
 import { test, expect } from "@playwright/test";
 
-test("Sign up successfully", async ({ request }) => {
-    const url = "https://conduit-api.bondaracademy.com/api/users/";
+const baseUrl = "https://conduit-api.bondaracademy.com/api";
+let token: string;
+let slug: string;
+let email: string;
+let password = "Test123@";
+let cmtTotal: any[] = [];
 
-    const random = Date.now()
-    const response = await request.post(url, {
-        data: {
-            "user": {
-                "email": `thucute${random}@mailsac.com`,
-                "password": "Test123@",
-                "username": `thucute${random}`
+test.describe("Exercise 11", async () => {
+
+    test("Test 1: Sign up successfully", async ({ request }) => {
+        const random = Date.now()
+        const response = await request.post(`${baseUrl}/users`, {
+            data: {
+                "user": {
+                    "email": `thucute${random}@mailsac.com`,
+                    "password": `${password}`,
+                    "username": `thucute${random}`
+                }
             }
-        }
+        });
+        //Verify status code
+        expect(response.status()).toBe(201);
+
+        //Verify response data
+        const Body = await response.json();
+
+        //Retrieve email and password
+        email = Body.user.email;
+
     });
 
-    console.log(`Status code: ${response.status()}`);
-    expect(response.status()).toBe(201);
+    test("Test 2: Login and create an article", async ({ request }) => {
+        await test.step("Login", async () => {
+            const response = await request.post(`${baseUrl}/users/login`, {
+                data: {
+                    "user": {
+                        "email": `${email}`,
+                        "password": "Test123@"
+                    }
+                }
+            });
+            //Verify status code
+            expect(response.status()).toBe(200);
 
-    const responseBody = await response.json();
-    console.log(responseBody);
-})
+            //retrieve token
+            const responseBody = await response.json();
+            token = await responseBody.user.token;
+        });
 
+        await test.step("Create an article", async () => {
+            const response = await request.post(`${baseUrl}/articles`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                data: {
+                    "article": {
+                        "title": "API in Playwright testing 890",
+                        "description": "How to use Playwright to create article 890",
+                        "body": "Article description sample 890",
+                        "tagList": [
+                            "Playwright Viet , pw, pw-k10 890"
+                        ]
+                    }
+                }
+            });
+            // Verify status code
+            expect(response.status()).toBe(201);
+            //Retrieve slug
+            const Body2 = await response.json();
+            slug = Body2.article.slug;
+        });
+    });
+
+    test("Test 3: Add 5 comments in Article", async ({ request }) => {
+        for (let i = 1; i <= 5; i++) {
+            const response = await request.post(`${baseUrl}/articles/${slug}/comments`, {
+                headers: {
+                    authorization: `Bearer ${token}`
+                },
+                data: {
+                    "comment": {
+                        "body": `Comment 0${i}`
+                    }
+                }
+            });
+            //Verify status code
+            expect(response.status()).toBe(200);
+
+            //Verify response data: add 5 cmts
+            const Body3 = await response.json();
+            cmtTotal.push(Body3.comment.id);
+            expect(cmtTotal.length).toBe(5);
+        };
+    });
+
+    test("Test 4: Delete cmt 2 and cmt 5 successfully", async ({ request }) => {
+        await test.step("Delete cmt 2 and cmt 5", async () => {
+            //Retrieve comment 2 and comment 5
+            const deletedCmt = [cmtTotal[1], cmtTotal[4]];
+
+            for (const id of deletedCmt) {
+                const response = await request.delete(`${baseUrl}/articles/${slug}/comments`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                //Verify status code
+                expect(response.status()).toBe(200);
+            };
+        });
+
+        test("Test 6: Xóa article đã tạo ở test 2", async ({ request }) => {
+            await test.step("Delete an article", async () => {
+                const response = await request.delete(`${baseUrl}/articles/${slug}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                });
+                //Verify status code
+                expect(response.status()).toBe(204);
+            });
+        });
+    });
+});
