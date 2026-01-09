2aea69e9004c3fb5b7ffd8c02d4a0cd77cded25f
Thu <minhthuphan2002@gmail.com>
2026-01-09 17:08:31 +0700

    feat: bai tap lesson 13: object destructure

--- tests/lesson-13-fixture-env-destructure/01-object-destructure.ts
+++ tests/lesson-13-fixture-env-destructure/01-object-destructure.ts
@@ -1,26 +1,31 @@
 /*
 1. Cho đối tượng person sau:
-const person = {
-firstName: "John",
-lastName: "Doe",
-age: 30,
-occupation: "Software Engineer"
-};
 Sử dụng Object Destructuring để trích xuất các thuộc tính firstName, lastName
-và age vào các biến riêng biệt.
-Hiển thị các giá trị của biến vừa khai báo trên console.
+và age vào các biến riêng biệt. Hiển thị các giá trị của biến vừa khai báo trên console.
 */
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    occupation: "Software Engineer"
+};
+
+const { firstName, lastName, age } = person;
+console.log(firstName, lastName, age);
 
 /*
 2. Cho một object car với các thuộc tính brand, model, year, color. Hãy sử dụng
 object destructuring để lấy ra 4 thuộc tính này và in ra console.
-const car = {
-brand: "Toyota",
-model: "Camry",
-year: 2022,
-color: "White",
-};
 */
+const car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2022,
+    color: "White",
+};
+
+const { brand, model, year, color } = car;
+console.log(brand, model, year, color);
 
 /*
 3. Cho một object user với thuộc tính name. Hãy sử dụng object destructuring để
@@ -31,19 +36,33 @@ const user = {
 };
 */
 
+const user: any = {};
+
+const { userName = "Guest" } = user;
+console.log(userName);
+
 /*
 4. Cho một object product với thuộc tính price. Hãy sử dụng object destructuring để
 lấy ra thuộc tính price và đặt giá trị mặc định là 0 nếu thuộc tính price không tồn
 tại. In ra console giá trị của price
-const product = {
-// ...
-};
 */
+
+const product: any = {};
+
+const { price = 0 } = product;
+console.log(price);
 
 /*
 5. Cho một object book với thuộc tính title. Hãy sử dụng object destructuring để lấy
 ra thuộc tính title và đặt alias là bookTitle. In ra console giá trị của bookTitle.
 */
+
+const book = {
+    title: "Nhà giả kim"
+};
+
+const { title: bookTitle } = book;
+console.log(bookTitle);
 
 /*
 6. Cho một object movie với thuộc tính director. Hãy sử dụng object destructuring
@@ -51,11 +70,29 @@ ra thuộc tính title và đặt alias là bookTitle. In ra console giá trị 
 filmDirector
 */
 
+const movie = {
+    director: "James Cameron"
+}
+
+const { director: filmDirector } = movie;
+console.log(filmDirector);
+
 /*
 7. Cho một object person với thuộc tính address là một object chứa các thuộc tính
 street, city, country. Hãy sử dụng object destructuring để lấy ra thuộc tính street
 nằm sâu trong object address. In ra console giá trị của street
 */
+
+const person1 = {
+    address: {
+        street: "Phan Tay Ho",
+        city: "HCM",
+        country: "VN"
+    }
+};
+
+const { address: { street } } = person1;
+console.log(street);
 
 /*
 8. Cho một object product với thuộc tính details là một object chứa các thuộc tính
@@ -63,3 +100,14 @@ brand, model, color. Hãy sử dụng object destructuring để lấy ra thuộ
 nằm sâu trong object details. In ra console giá trị của model
 Playwright
 */
+
+const product1 = {
+    details: {
+        brand1: "Glamrr Q",
+        model1: "Lip balm",
+        color1: "05 - Plum'D"
+    }
+};
+
+const { details: { model1, color1 } } = product1;
+console.log(model1, color1);
