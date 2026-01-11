40c5cf147a838652513940998cccf50c5b3f8777
Thu <minhthuphan2002@gmail.com>
2026-01-11 22:44:24 +0700

    feat: remove env file

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ console.log(process.env.ENV1);
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -31,6 +31,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
